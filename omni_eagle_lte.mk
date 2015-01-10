b5e519a8eb43e71da52e49e4ca3a11df3830baf3
erikcas <erikcas1972@gmail.com>
2015-01-10 17:16:25 +0100

    eagle: reorganize (1/2)
    Change-Id: Ia1d9c650c9956f3434d09b31dfd3fc964f4c98bd

--- omni_eagle_lte.mk
+++ omni_eagle_lte.mk
@@ -1,18 +1,17 @@
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit Omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/eagle/full_eagle.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=D2303 \
+BUILD_FINGERPRINT=Sony/D2303/D2303:4.4.4/18.3.1.C.0.21/2n9_bg:user/release-keys \
+PRIVATE_BUILD_DESC="D2303-user 18.3.1.C.0.21 2n9_bg release-keys"
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := omni_eagle_lte
 PRODUCT_DEVICE := eagle_lte
-PRODUCT_BRAND := Sony
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_MODEL := Xperia M2 LTE
