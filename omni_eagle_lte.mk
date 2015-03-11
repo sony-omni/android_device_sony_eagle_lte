8df518f86e404c795a27dececd3cfeac0ac3ee18
erikcas <erikcas1972@gmail.com>
2015-03-11 13:14:30 +0100

    move mobile properties to device specific

--- omni_eagle_lte.mk
+++ omni_eagle_lte.mk
@@ -7,6 +7,11 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/sony/eagle/full_eagle.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.radio.multisim.config=dsds \
+   telephony.lteOnGsmDevice=1 \
+   ro.telephony.default_network=9
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_NAME=D2303 \
 BUILD_FINGERPRINT=Sony/D2303/D2303:4.4.4/18.3.1.C.0.21/2n9_bg:user/release-keys \
